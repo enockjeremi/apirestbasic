64af6ad987b17684d617b927542fe6def2df83c7
Enock Jeremi <enockjeremi@gmail.com>
2022-11-27 01:31:26 -0300

    Lineas para el uso de Cors comentadas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes')
 const mongoose = require('mongoose');
+// const cors = require('cors');
 
 
 const app = express();
@@ -13,7 +14,7 @@ mongoose.connect(`mongodb://localhost/${db}`, {
     useUnifiedTopology: true
 });
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
