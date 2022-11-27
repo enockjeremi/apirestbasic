7fc142f4e642026b7afef68b2f3008e3bd2ebe85
Enock Jeremi <enockjeremi@gmail.com>
2022-11-27 01:37:48 -0300

    Modelo basico en mongoose

---
+++ models/BasicModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BasicModelSchema = new Schema({
+    name: {
+        type: String,
+        trim: true
+    },
+    phone: {
+        type: String,
+        trim: true
+    },
+    email: {
+        type: String,
+        trim: true
+    }
+});
+
+module.exports = mongoose.model('BasicModelSchema', BasicModelSchema);
