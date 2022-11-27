dca97051ac06bc61641c5d164858f8e5e7861753
Enock Jeremi <enockjeremi@gmail.com>
2022-11-27 01:49:39 -0300

    Controllers agregados

---
+++ controllers/BasicController.js
@@ -0,0 +1,57 @@
+const { findById } = require('../models/BasicModel');
+const BasicModel = require('../models/BasicModel');
+
+exports.postBasicModel = async (req, res, next) => {
+    const basicmodel = new BasicModel(req.body);
+    try {
+        await basicmodel.save();
+        res.json(basicmodel);
+    } catch (error) {
+        console.log(error);
+        next();
+    };
+};
+
+exports.getBasicModels = async (req, res, next) => {
+    try {
+        const basicmodel = await BasicModel.find({});
+        res.json(basicmodel);
+    } catch (error) {
+        console.log(error);
+        next();
+    };
+};
+
+exports.getBasicModel = async (req, res, next) => {
+    try {
+        const basicmodel = await BasicModel.findById(req.params.id);
+        res.json(basicmodel);
+    } catch (error) {
+        console.log(error);
+        next();
+    };
+};
+
+exports.putBasicModel = async (req, res, next) => {
+    try {
+        const basicmodel = await BasicModel.findOneAndUpdate({id: req.params.id}, req.body, {
+            new: true
+        });
+        res.json(basicmodel);
+    } catch (error) {
+        console.log(error);
+        next();
+    };
+};
+
+exports.postBasicModel = async (req, res, next) => {
+    try {
+        await BasicModel.findOneAndDelete({id: req.params.id});
+        res.json({
+            mensaje: 'Se elimino correctamente'
+        })
+    } catch (error) {
+        console.log(error);
+        next();
+    };
+};
