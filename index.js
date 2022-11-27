2f8175cd6efef429e7850f57849f70e7f10ea47b
Enock Jeremi <enockjeremi@gmail.com>
2022-11-27 01:27:07 -0300

    Agregamos index.js y su contenido para la conexion a la DB y el arranque de el server, ademas agregamos un archivo de rutas

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const routes = require('./routes')
+const mongoose = require('mongoose');
+
+
+const app = express();
+const port = process.env.PORT || 4000;
+const db = 'marines';
+
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://localhost/${db}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/', routes());
+
+app.listen(port, () => {
+    console.log('Server Online: ' + port);
+})
